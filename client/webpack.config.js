fd2d90f26b4a0df79c77eee9a230a0e6bf899f86
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2019-01-31 22:31:00 -0800

    Initial commit. Install dependencies. Structure dependencies

---
+++ client/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    devtool: "cheap-module-inline-source-map",
+    entry: ["babel-polyfill", "./src/app.js"],
+
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+
+    module: {
+        rules: [
+            { test: /\.js$/, loader: "babel-loader", exclude: /node_modules/ },
+            {
+                test: /\.css$/,
+                loader: "style-loader"
+            },
+            {
+                test: /\.css$/,
+                loader: "css-loader",
+                query: {
+                    modules: true,
+                    localIdentName: "[name]__[local]___[hash:base64:5]"
+                }
+            },
+
+            { test: /\.(png|jpg|svg)$/, loader: "url-loader?limit=8192" }
+        ]
+    },
+
+    devServer: {
+        historyApiFallback: true
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/public/index.html"
+        })
+    ]
+};
