99554f0b4c3ea561c3d9cd1c287a355e3a619627
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2019-02-03 14:13:37 -0800

    Add currencyDataEndpoint function

--- client/src/api/endpoints.js
+++ client/src/api/endpoints.js
@@ -1,3 +1,17 @@
 import key from "./key";
 
 export const topTenEndpoint = `https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10&tsym=USD&api_key=${key}`;
+
+export const currencyDataEndpoint = (currency = "", timeline = "month") => {
+    let limit;
+
+    timeline = timeline.toLowerCase();
+
+    if (timeline === "week") limit = 7;
+
+    if (timeline === "month") limit = 30;
+
+    if (timeline === "year") limit = 365;
+
+    return `https://min-api.cryptocompare.com/data/histoday?fsym=${currency}&tsym=USD&limit=${limit}`;
+};
