02a9194fc75f779864a647714510e8bb3d937b9d
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2019-02-03 14:12:49 -0800

    Have the value of the currentlyViewedCurrency state property equal an object with both currencyInfo and historicalData properties. Set up both the updateCurrentlyViewedCurrency and updateTimeLine methods to update state object.

--- client/src/components/Currencies/CurrenciesContainer.js
+++ client/src/components/Currencies/CurrenciesContainer.js
@@ -1,16 +1,21 @@
 import React, { Component } from "react";
 import Currencies from "./Currencies";
+import { topTenEndpoint, currencyDataEndpoint } from "API/endpoints";
 
 class CurrenciesContainer extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            currentlyViewedCurrency: "",
+            currentlyViewedCurrency: {
+                currencyInfo: {},
+                historicalData: []
+            },
             timeline: "month",
             currencies: [],
 
             fetchingCurrencies: true,
+            fetchingCurrencyData: true,
             errorFetchingCurrencies: false
         };
 
@@ -22,40 +27,148 @@ class CurrenciesContainer extends Component {
     }
 
     /**
-     *  Fetches currencies based on API endpoint
+     * If the currencies array was provided via props,
+     * update state with the given currencies.
+     * Otherwise, fetch the top 10 currencies is
+     * provided via props.
+     *
      *  @param
      *  @return
      */
     componentDidMount() {
         this.setState({ fetchingCurrencies: true });
+        if (this.props.currencies) {
+            this.setState({
+                currencies: this.props.currencies,
+                fetchingCurrencies: false
+            });
+
+            this.updateCurrentlyViewedCurrency(this.state.currencies[0].Name);
+        } else {
+            (async () => {
+                try {
+                    let topTenCurrenciesResponse = await fetch(topTenEndpoint);
+                    let topTenParsedResponse = await topTenCurrenciesResponse.json();
+
+                    let coinInfoArr = [];
+
+                    for (let i = 0; i < topTenParsedResponse.Data.length; i++) {
+                        coinInfoArr.push(topTenParsedResponse.Data[i].CoinInfo);
+                    }
+
+                    this.setState({
+                        currencies: coinInfoArr,
+                        fetchingCurrencies: false
+                    });
+
+                    this.updateCurrentlyViewedCurrency(coinInfoArr[0]);
+                } catch (err) {
+                    console.error(err);
+
+                    this.setState({
+                        fetchingCurrencies: false,
+                        errorFetchingCurrencies: true
+                    });
+                }
+            })();
+        }
+    }
+
+    /**
+     * Updates the currently viewed currency when user clicks on
+     * specific currency from the list.
+     * Fetches historical data for the currency
+     *
+     * @param Object currency
+     * @return
+     */
+    updateCurrentlyViewedCurrency(currency) {
+        /*
+            If current currency is the same as the one that user clicked,
+            immediately return
+        */
+        if (
+            currency.Name ===
+            this.state.currentlyViewedCurrency.currencyInfo.Name
+        )
+            return;
+
+        this.setState({ fetchingCurrencyData: true });
 
         (async () => {
             try {
-                let response = await fetch(this.props.endpoint);
-                let parsedResponse = await response.json();
+                let historicalCurrencyResponse = await fetch(
+                    currencyDataEndpoint(currency.Name, this.state.timeline)
+                );
+
+                let parsedHistoricalData = await historicalCurrencyResponse.json();
 
                 this.setState({
-                    currencies: parsedResponse.Data,
-                    currentCurrency: parsedResponse.Data[0],
-                    fetchingCurrencies: false
+                    currentlyViewedCurrency: {
+                        currencyInfo: currency,
+                        historicalData: parsedHistoricalData.Data
+                    },
+                    fetchingCurrencyData: false
                 });
             } catch (err) {
+                console.error(err);
+
                 this.setState({
-                    fetchingCurrencies: false,
+                    fetchingCurrencyData: false,
+                    fetchingCurrencyData: false,
                     errorFetchingCurrencies: true
                 });
             }
         })();
     }
 
-    updateCurrentlyViewedCurrency(currency) {
-        // ajax request here
-        this.setState({ currentlyViewedCurrency: currency });
-    }
-
+    /**
+     * Updates the currently viewed currency when user clicks on
+     * specific currency from the list.
+     * Fetches historical data for the currency
+     *
+     * @param Object currency
+     * @return
+     */
     updateTimeLine(timeline) {
-        // ajax request here
-        this.setState({ timeline });
+        /*
+            If current timeline is the same as the one that user clicked,
+            immediately return
+        */
+        if (timeline === this.state.timeline) return;
+
+        this.setState({ fetchingCurrencyData: true });
+
+        (async () => {
+            try {
+                let historicalCurrencyResponse = await fetch(
+                    currencyDataEndpoint(
+                        this.state.currentlyViewedCurrency.currencyInfo,
+                        timeline
+                    )
+                );
+
+                let parsedHistoricalData = await historicalCurrencyResponse.json();
+
+                this.setState(state => {
+                    return {
+                        currentlyViewedCurrency: {
+                            historicalData: parsedHistoricalData.Data
+                        },
+                        timeline,
+                        fetchingCurrencyData: false
+                    };
+                });
+            } catch (err) {
+                console.error(err);
+
+                this.setState({
+                    fetchingCurrencyData: false,
+                    fetchingCurrencyData: false,
+                    errorFetchingCurrencies: true
+                });
+            }
+        })();
     }
 
     render() {
