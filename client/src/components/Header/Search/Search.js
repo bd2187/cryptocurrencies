b951dee8a5c77fc245dbfb57ca6cee0b1320d851
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2019-02-05 21:01:11 -0800

    Display searchResults provided by props. Debounce invocation to updateQuery function provided by props

--- client/src/components/Header/Search/Search.js
+++ client/src/components/Header/Search/Search.js
@@ -10,17 +10,46 @@ class Search extends Component {
         this.onChange = this.onChange.bind(this);
     }
 
+    /**
+     * Changes searchValue in state object. Debounces invocation
+     * of updateQuery function provided via props.
+     *
+     * @param Object evt
+     * @return
+     */
     onChange(evt) {
         this.setState({ searchValue: evt.target.value });
+        var value = evt.target.value;
+
+        let timerId;
+
+        if (timerId) clearTimeout(timerId);
+
+        /*
+            Debounce invocation so it is not called
+            after each keystroke
+        */
+        timerId = setTimeout(() => {
+            this.props.updateQuery(value);
+            timerId = null;
+        }, 1000);
     }
 
     render() {
+        const { searchResults } = this.props;
         return (
-            <input
-                type="text"
-                value={this.state.value}
-                onChange={this.onChange}
-            />
+            <div>
+                <input
+                    type="text"
+                    value={this.state.value}
+                    onChange={this.onChange}
+                />
+                <ul>
+                    {searchResults.map(function(result) {
+                        return <li key={result.FullName}>{result.FullName}</li>;
+                    })}
+                </ul>
+            </div>
         );
     }
 }
