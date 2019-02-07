efdb1bc99caa074ef9198fcda8dbf922eba6234d
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2019-02-06 20:47:46 -0800

    Import the CurrencyProfileContainer. Render it when the search path is visited

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,27 +1,30 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Dashboard from "./Dashboard/Dashboard";
+import Header from "./Header/Header";
+import CurrencyProfileContainer from "./CurrencyProfile/CurrencyProfileContainer";
 const Profile = () => <h1>Profile</h1>; // shows favorites
-const Search = () => <h1>Search</h1>;
 const SearchedCurrency = () => <h1>Searched Currency</h1>;
 const PageNotFound = () => <h1>404 not found</h1>;
 
 const App = () => {
     return (
-        <Router>
-            <Switch>
-                <Route exact path="/" component={Dashboard} />
-                <Route exact path="/profile" component={Profile} />
-                <Route exact path="/search" component={Search} />
-                <Route
-                    exact
-                    path="/search/:currency"
-                    component={SearchedCurrency}
-                />
-                <Route component={PageNotFound} />
-            </Switch>
-        </Router>
+        <Fragment>
+            <Header />
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={Dashboard} />
+                    <Route exact path="/profile" component={Profile} />
+                    <Route
+                        exact
+                        path="/search/:currency"
+                        component={CurrencyProfileContainer}
+                    />
+                    <Route component={PageNotFound} />
+                </Switch>
+            </Router>
+        </Fragment>
     );
 };
 
