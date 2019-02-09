2d1b3750b6ee654f9fc0150f3f930817d200d828
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2019-02-09 12:06:32 -0800

    Add props param

--- client/src/components/CurrencyProfile/CurrencyProfile.js
+++ client/src/components/CurrencyProfile/CurrencyProfile.js
@@ -1,7 +1,18 @@
 import React from "react";
 
-const CurrencyProfile = () => {
-    return <h1>Currency Profile</h1>;
+const CurrencyProfile = ({
+    currencyData,
+    fetchingCurrencyData,
+    errorFetchingData
+}) => {
+    if (fetchingCurrencyData) {
+        return <h1>loading</h1>;
+    } else if (errorFetchingData) {
+        return <h1>error</h1>;
+    } else {
+        console.warn(currencyData);
+        return <h1>Currency Profile</h1>;
+    }
 };
 
 export default CurrencyProfile;
