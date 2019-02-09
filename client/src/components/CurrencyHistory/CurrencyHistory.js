c094ac04a316f18cf426ef2bfe7ed079b9f818d9
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2019-02-09 15:39:34 -0800

    Initial commit

---
+++ client/src/components/CurrencyHistory/CurrencyHistory.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const CurrencyHistory = ({
+    historicalCurrencyData,
+    updateTimeline,
+    fetchingCurrencyData,
+    fetchingCurrencyError
+}) => {
+    return (
+        <div>
+            {/* <p>{currencyInfo.Name}</p>
+            <img
+                style={{ width: "50px" }}
+                src={`http://cryptocompare.com/${currencyInfo.ImageUrl}`}
+            /> */}
+        </div>
+    );
+};
+
+export default CurrencyHistory;
