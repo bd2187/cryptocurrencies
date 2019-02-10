e97cd3c9084f7577916d9664d82dcbd2986c26ff
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2019-02-09 17:08:41 -0800

    Create sample markup. to view data

--- client/src/components/CurrencyProfile/CurrencyProfile.js
+++ client/src/components/CurrencyProfile/CurrencyProfile.js
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { Fragment } from "react";
+import CurrentHistoryContainer from "../CurrencyHistory/CurrencyHistoryContainer";
 
 const CurrencyProfile = ({
-    currencyData,
+    currencyInfo,
+    currencyPrice,
     fetchingCurrencyData,
     errorFetchingData
 }) => {
-    if (fetchingCurrencyData) {
+    if (fetchingCurrencyData || !currencyPrice.hasOwnProperty("USD")) {
         return <h1>loading</h1>;
     } else if (errorFetchingData) {
         return <h1>error</h1>;
     } else {
-        console.warn(currencyData);
-        return <h1>Currency Profile</h1>;
+        return (
+            <Fragment>
+                <CurrentHistoryContainer
+                    currentlyViewedCurrency={currencyInfo}
+                />
+                <h1>Currency Profile</h1>
+                <p>{currencyPrice.USD.FROMSYMBOL}</p>
+                <p>{currencyPrice.USD.HIGH24HOUR}</p>
+            </Fragment>
+        );
     }
 };
 
