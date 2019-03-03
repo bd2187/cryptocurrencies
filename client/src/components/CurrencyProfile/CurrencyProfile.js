b54c6ca63f24891bd4b04e54bb46d6aefb32caf4
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2019-03-03 10:17:51 -0800

    Create CurrencyTable component to render curreny details

--- client/src/components/CurrencyProfile/CurrencyProfile.js
+++ client/src/components/CurrencyProfile/CurrencyProfile.js
@@ -1,6 +1,49 @@
-import React, { Fragment } from "react";
+import React from "react";
 import Loading from "../Loading/Loading";
 import CurrentHistoryContainer from "../CurrencyHistory/CurrencyHistoryContainer";
+import styles from "./CurrencyProfile.css";
+
+const CurrencyTable = ({ currencyInfo, currencyPrice }) => {
+    return (
+        <table className={styles["currency-table"]}>
+            <tbody>
+                <tr className={styles["currency-table-row"]}>
+                    <td>Price:</td>
+                    <td>{currencyPrice.USD.PRICE}</td>
+                </tr>
+
+                <tr className={styles["currency-table-row"]}>
+                    <td>Market Rank:</td>
+                    <td>{currencyInfo.SortOrder}</td>
+                </tr>
+
+                <tr className={styles["currency-table-row"]}>
+                    <td>Market Cap:</td>
+                    <td>{currencyPrice.USD.MKTCAP}</td>
+                </tr>
+
+                <tr className={styles["currency-table-row"]}>
+                    <td>Total Supply:</td>
+                    <td>{currencyPrice.USD.SUPPLY}</td>
+                </tr>
+
+                <tr className={styles["currency-table-row"]}>
+                    <td>Today's High/Low</td>
+                    <td>
+                        {`${currencyPrice.USD.HIGH24HOUR}/${
+                            currencyPrice.USD.LOW24HOUR
+                        }`}
+                    </td>
+                </tr>
+
+                <tr className={styles["currency-table-row"]}>
+                    <td>Change % Last 24 Hours:</td>
+                    <td>{currencyPrice.USD.CHANGEPCT24HOUR}</td>
+                </tr>
+            </tbody>
+        </table>
+    );
+};
 
 const CurrencyProfile = ({
     currencyInfo,
@@ -24,10 +67,7 @@ const CurrencyProfile = ({
         }
 
         return (
-            <Fragment>
-                <h1>Currency Profile</h1>
-                <p>{currencyPrice.USD.FROMSYMBOL}</p>
-                <p>{currencyPrice.USD.HIGH24HOUR}</p>
+            <div className={styles["currency-profile"]}>
                 {currentlyViewedCurrency ? (
                     <CurrentHistoryContainer
                         currentlyViewedCurrency={currentlyViewedCurrency}
@@ -35,7 +75,11 @@ const CurrencyProfile = ({
                 ) : (
                     <Loading text={"Loading Currency Data"} ms={200} />
                 )}
-            </Fragment>
+                <CurrencyTable
+                    currencyInfo={currencyInfo}
+                    currencyPrice={currencyPrice}
+                />
+            </div>
         );
     }
 };
