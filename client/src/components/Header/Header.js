7b82b3fe9bb346dc7e11b606974b0ee6d63f4a30
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2019-03-03 14:55:27 -0800

    Add link to homepage

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,8 +1,17 @@
 import React from "react";
+import styles from "./Header.css";
 import SearchContainer from "./Search/SearchContainer";
+import { Link } from "react-router-dom";
 
 const Header = ({ allCoins }) => {
-    return <SearchContainer allCoins={allCoins} />;
+    return (
+        <div className={styles.header}>
+            <Link to="/" className={styles["homepage-link"]}>
+                CryptoCurrencies
+            </Link>
+            <SearchContainer allCoins={allCoins} />
+        </div>
+    );
 };
 
 export default Header;
