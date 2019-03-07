0d8da9c7d3c5626b21cf3e7c917380b69336dba4
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2019-03-06 21:54:56 -0800

    Convert component to class component. Create toggleLoading method

--- client/src/components/Loader/Loader.js
+++ client/src/components/Loader/Loader.js
@@ -1,28 +1,53 @@
-import React from "react";
+import React, { Component } from "react";
 import styles from "./Loader.css";
 
-const Loader = ({ isLoading, children }) => {
-    const displayLoader = () => {
-        return isLoading ? styles["display-loading-spinner"] : "";
-    };
+class Loader extends Component {
+    constructor(props) {
+        super(props);
+        this.loaderRef = React.createRef();
+        this.toggleLoading = this.toggleLoading.bind(this);
+    }
 
-    console.warn(isLoading);
-    return (
-        <div className={styles["loader"]}>
-            {children}
+    toggleLoading(isLoading) {
+        if (isLoading && this.loaderRef.current) {
+            this.loaderRef.current.classList.add(
+                styles["display-loading-spinner"]
+            );
+        } else {
+            setTimeout(() => {
+                this.loaderRef.current.classList.remove(
+                    styles["display-loading-spinner"]
+                );
+            }, 1000);
+        }
+    }
 
+    render() {
+        const { isLoading, children } = this.props;
+
+        this.toggleLoading(isLoading);
+
+        return (
             <div
-                className={`${
-                    styles["ball-loader-container"]
-                } ${displayLoader()}`}
+                className={`${styles["loader"]} ${
+                    styles["display-loading-sip"]
+                }`}
             >
-                <div className={styles["ball-loader"]}>
-                    <div className={styles["ball-bounce1"]} />
-                    <div className={styles["ball-bounce2"]} />
+                {children}
+                <div
+                    className={`${styles["ball-loader-container"]} ${
+                        styles["display-loading-spinner"]
+                    }`}
+                    ref={this.loaderRef}
+                >
+                    <div className={styles["ball-loader"]}>
+                        <div className={styles["ball-bounce1"]} />
+                        <div className={styles["ball-bounce2"]} />
+                    </div>
                 </div>
             </div>
-        </div>
-    );
-};
+        );
+    }
+}
 
 export default Loader;
