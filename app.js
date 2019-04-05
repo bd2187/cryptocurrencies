90b82af80465013cd5e5b6e37f8a6654976f99cb
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2019-04-04 19:18:55 -0700

    Remove express import. Require http module and create server with module. In the createServer cb funciton, set a condition for the /top-ten url. In the condition contact third-party API for top-ten cryptocurrencies

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-const express = require("express");
-const app = express();
+const http = require("http");
+const fetch = require("node-fetch");
+const key = require("./config/key");
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.warn(`Now listening to port: ${port}`));
+const PORT = process.env.PORT || 5000;
+
+http.createServer((req, res) => {
+    if (req.url === "/top-ten") {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        fetch(
+            `https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10&tsym=USD&api_key=${key()}`
+        )
+            .then(fetchedRes => {
+                return fetchedRes.json();
+            })
+            .then(parsedRes => {
+                res.write(JSON.stringify(parsedRes));
+            });
+    }
+}).listen(PORT, () => {
+    console.warn(`Not listening to port ${PORT}`);
+});
