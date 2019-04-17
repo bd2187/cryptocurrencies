f6d3ad7a59a4210035947ce89b53c94fd15daa49
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2019-04-16 20:29:03 -0700

    Set up respone header in middleware

--- app.js
+++ app.js
@@ -7,6 +7,19 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5000");
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+
 // Routes
 const coinsRoutes = require("./routes/currencies");
 
