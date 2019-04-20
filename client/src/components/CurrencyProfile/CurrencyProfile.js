092794f9c07e6f15fcaac9639e03a8bc04eac46b
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2019-04-20 08:36:34 -0700

    Add BEM class names

--- client/src/components/CurrencyProfile/CurrencyProfile.js
+++ client/src/components/CurrencyProfile/CurrencyProfile.js
@@ -6,41 +6,87 @@ import styles from "./CurrencyProfile.css";
 const CurrencyTable = ({ currencyInfo, currencyPrice }) => {
     return (
         <Loader>
-            <h1 className={styles["currency-name"]}>{currencyInfo.FullName}</h1>
-            <table className={styles["currency-table"]}>
+            <h1 className={styles["currencyName"]}>{currencyInfo.FullName}</h1>
+            <table className={styles["currencyTable"]}>
                 <tbody>
-                    <tr className={styles["currency-table-row"]}>
-                        <td>Price:</td>
-                        <td>{currencyPrice.USD.PRICE}</td>
+                    <tr className={styles["currencyTableRow"]}>
+                        <td className={styles["currencyTableRow__data"]}>
+                            Price:
+                        </td>
+                        <td
+                            className={`${styles["currencyTableRow__data"]} ${
+                                styles["currencyTableRow__value"]
+                            }`}
+                        >
+                            {currencyPrice.USD.PRICE}
+                        </td>
                     </tr>
 
-                    <tr className={styles["currency-table-row"]}>
-                        <td>Market Rank:</td>
-                        <td>{currencyInfo.SortOrder}</td>
+                    <tr className={styles["currencyTableRow"]}>
+                        <td className={styles["currencyTableRow__data"]}>
+                            Market Rank:
+                        </td>
+                        <td
+                            className={`${styles["currencyTableRow__data"]} ${
+                                styles["currencyTableRow__value"]
+                            }`}
+                        >
+                            {currencyInfo.SortOrder}
+                        </td>
                     </tr>
 
-                    <tr className={styles["currency-table-row"]}>
-                        <td>Market Cap:</td>
-                        <td>{currencyPrice.USD.MKTCAP}</td>
+                    <tr className={styles["currencyTableRow"]}>
+                        <td className={styles["currencyTableRow__data"]}>
+                            Market Cap:
+                        </td>
+                        <td
+                            className={`${styles["currencyTableRow__data"]} ${
+                                styles["currencyTableRow__value"]
+                            }`}
+                        >
+                            {currencyPrice.USD.MKTCAP}
+                        </td>
                     </tr>
 
-                    <tr className={styles["currency-table-row"]}>
-                        <td>Total Supply:</td>
-                        <td>{currencyPrice.USD.SUPPLY}</td>
+                    <tr className={styles["currencyTableRow"]}>
+                        <td className={styles["currencyTableRow__data"]}>
+                            Total Supply:
+                        </td>
+                        <td
+                            className={`${styles["currencyTableRow__data"]} ${
+                                styles["currencyTableRow__value"]
+                            }`}
+                        >
+                            {currencyPrice.USD.SUPPLY}
+                        </td>
                     </tr>
 
-                    <tr className={styles["currency-table-row"]}>
-                        <td>Today's High/Low</td>
-                        <td>
+                    <tr className={styles["currencyTableRow"]}>
+                        <td className={styles["currencyTableRow__data"]}>
+                            Today's High/Low
+                        </td>
+                        <td
+                            className={`${styles["currencyTableRow__data"]} ${
+                                styles["currencyTableRow__value"]
+                            }`}
+                        >
                             {`${currencyPrice.USD.HIGH24HOUR}/${
                                 currencyPrice.USD.LOW24HOUR
                             }`}
                         </td>
                     </tr>
 
-                    <tr className={styles["currency-table-row"]}>
-                        <td>Change % Last 24 Hours:</td>
-                        <td>{currencyPrice.USD.CHANGEPCT24HOUR}</td>
+                    <tr className={styles["currencyTableRow"]}>
+                        <td className={styles["currencyTableRow__data"]}>
+                            Change % Last 24 Hours:
+                        </td>
+                        <td
+                            className={`${styles["currencyTableRow__data"]} ${
+                                styles["currencyTableRow__value"]
+                            }`}
+                        >
+                            {currencyPrice.USD.CHANGEPCT24HOUR}
+                        </td>
                     </tr>
                 </tbody>
             </table>
@@ -62,7 +108,7 @@ const CurrencyProfile = ({
         return null;
     } else if (errorFetchingData) {
         return (
-            <h1 className={styles["error-message"]}>Error: {errorMessage}</h1>
+            <h1 className={styles["errorMessage"]}>Error: {errorMessage}</h1>
         );
     } else {
         let currentlyViewedCurrency = null;
@@ -76,8 +122,8 @@ const CurrencyProfile = ({
         }
 
         return (
-            <div className={styles["currency-profile"]}>
-                <div className={styles["current-history-container"]}>
+            <div className={styles["currencyProfile"]}>
+                <div className={styles["currencyProfile__historyContainer"]}>
                     {currentlyViewedCurrency ? (
                         <CurrentHistoryContainer
                             currentlyViewedCurrency={currentlyViewedCurrency}
